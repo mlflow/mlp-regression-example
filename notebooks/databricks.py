433bc11c36e2d3cdd63a2dc6cb4a93f336acaaba
Mingyu Li <mingyu.li@databricks.com>
2022-09-29 11:44:51 -0700

    Copy files from mlp-regression-template repo
    Signed-off-by: Mingyu Li <mingyu.li@databricks.com>

---
+++ notebooks/databricks.py
@@ -0,0 +1,66 @@
+# Databricks notebook source
+
+# MAGIC %md
+# MAGIC # MLflow Regression Pipeline Databricks Notebook
+# MAGIC This notebook runs the MLflow Regression Pipeline on Databricks and inspects its results.
+# MAGIC
+# MAGIC For more information about the MLflow Regression Pipeline, including usage examples,
+# MAGIC see the [Regression Pipeline overview documentation](https://mlflow.org/docs/latest/pipelines.html#regression-pipeline)
+# MAGIC and the [Regression Pipeline API documentation](https://mlflow.org/docs/latest/python_api/mlflow.pipelines.html#module-mlflow.pipelines.regression.v1.pipeline).
+
+# COMMAND ----------
+
+# MAGIC %pip install mlflow[pipelines]
+# MAGIC %pip install -r ../requirements.txt
+
+# COMMAND ----------
+
+from mlflow.pipelines import Pipeline
+
+p = Pipeline(profile="databricks")
+
+# COMMAND ----------
+
+p.clean()
+
+# COMMAND ----------
+
+p.inspect()
+
+# COMMAND ----------
+
+p.run("ingest")
+
+# COMMAND ----------
+
+p.run("split")
+
+# COMMAND ----------
+
+p.run("transform")
+
+# COMMAND ----------
+
+p.run("train")
+
+# COMMAND ----------
+
+p.run("evaluate")
+
+# COMMAND ----------
+
+p.run("register")
+
+# COMMAND ----------
+
+p.inspect("train")
+
+# COMMAND ----------
+
+training_data = p.get_artifact("training_data")
+training_data.describe()
+
+# COMMAND ----------
+
+trained_model = p.get_artifact("model")
+print(trained_model)
