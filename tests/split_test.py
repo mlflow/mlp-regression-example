d90811c7ed8313ffc81e9243b291703c48cb8311
Mingyu Li <mingyu.li@databricks.com>
2022-10-27 17:59:00 -0700

    Update split test and run reformat
    Signed-off-by: Mingyu Li <mingyu.li@databricks.com>

--- tests/split_test.py
+++ tests/split_test.py
@@ -1,8 +1,8 @@
 import pytest
 import os
 import pandas as pd
-from pandas import DataFrame
-from steps.split import process_splits
+from pandas import Series
+from steps.split import create_dataset_filter
 
 
 @pytest.fixture
@@ -12,25 +12,12 @@ def sample_data():
     )
 
 
-def test_post_split_fn_returns_datasets_with_correct_spec(sample_data):
+def test_split_step_outcome(sample_data):
     train = sample_data[0:3]
     validation = sample_data[4:7]
     test = sample_data[7:10]
-    (train_processed, validation_processed, test_processed) = process_splits(
-        train, validation, test
-    )
-    assert isinstance(train_processed, DataFrame)
-    assert isinstance(validation_processed, DataFrame)
-    assert isinstance(test_processed, DataFrame)
-
-
-def test_post_split_fn_returns_non_empty_datasets(sample_data):
-    train = sample_data[0:3]
-    validation = sample_data[4:7]
-    test = sample_data[7:10]
-    (train_processed, validation_processed, test_processed) = process_splits(
-        train, validation, test
-    )
-    assert not train_processed.empty
-    assert not validation_processed.empty
-    assert not test_processed.empty
+    processed = create_dataset_filter(sample_data)
+    assert isinstance(processed, Series)
+    assert not processed.empty
+    assert sample_data.size == 60
+    assert sample_data[processed].size == 60
